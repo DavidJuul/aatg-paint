fd5a4c1d1dcfe8ae047d70eab8aecdf2352ea250
AgentFluffy <Fbn212@hotmail.dk>
2021-03-12 14:29:23 +0100

    Add MenuStrip functionality

--- Paint/Form1.cs
+++ Paint/Form1.cs
@@ -15,6 +15,30 @@ namespace Paint
         public Form1()
         {
             InitializeComponent();
+
+
+            // ---------------- Main menu that holds multiple menu items -------
+            MenuStrip mainMenu = new MenuStrip();
+
+            // fileMenu colors
+            mainMenu.BackColor = Color.LightGray;
+            mainMenu.ForeColor = Color.Black;
+
+            mainMenu.Font = new Font("Roboto", 16);
+            mainMenu.Text = "Menu";
+
+            this.MainMenuStrip = mainMenu;
+            Controls.Add(mainMenu);
+            // ---------------- end -------
+
+
+            // Add ToolStripMenuItem to the MainMenu with properties
+            ToolStripMenuItem fileMenu = new ToolStripMenuItem();
+
+            fileMenu.Text = "Filer";
+
+            mainMenu.Items.Add(fileMenu);
+
         }
     }
 }
